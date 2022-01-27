821ff48f615b8a80966a7116626f1b492ca03e61
cahito <cahito@gmail.com>
2022-01-26 22:31:38 -0300

    Quinto requisito cumprido com lint feito

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,22 +1,34 @@
 const data = require('../data/zoo_data');
 
+const total = {
+  lions: 4,
+  tigers: 2,
+  bears: 3,
+  penguins: 4,
+  otters: 4,
+  frogs: 2,
+  snakes: 2,
+  elephants: 4,
+  giraffes: 6,
+};
+
+const acheBixo = (parametro) => data.species.find(({ name }) => parametro.specie === name);
+
 function countAnimals(animal) {
   if (!animal) {
-    return {
-      lions: 4, tigers: 2, bears: 3, penguins: 4, otters: 4, frogs: 2, snakes: 2, elephants: 4, giraffes: 6,
-    };
+    return total;
   }
   if (!animal.sex) {
-    return data.species.find(({ name }) => animal.specie === name).residents.reduce((acc, cur) => ((acc !== cur) ? acc + 1 : acc), 0);
+    return acheBixo(animal).residents.length;
   }
-  if (animal.sex) {
-    return data.species.find(({ name }) => animal.specie === name).residents.reduce((acc, cur) => ((cur.sex === animal.sex) ? acc + 1 : acc), 0);
-  }
+  return acheBixo(animal).residents.reduce((ac, cu) => ((cu.sex === animal.sex) ? ac + 1 : ac), 0);
 }
 
 module.exports = countAnimals;
 
-console.log(countAnimals({ specie: 'penguins', sex: 'male' }));
+// reduce((acc, cur) => ((acc !== cur) ? acc + 1 : acc), 0)
+
+// console.log(countAnimals({ specie: 'otters', sex: 'male' }));
 
 // let teste = { test1: 'coisa', test2: 'nenhuma' };
 
