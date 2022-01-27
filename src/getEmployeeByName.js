5063b940aae7663fcc0d08002ba91805e82d0915
cahito <cahito@gmail.com>
2022-01-27 13:09:46 -0300

    Nono requisito cumprido, e comprido!

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -3,12 +3,15 @@ const data = require('../data/zoo_data');
 const { employees } = data;
 
 function getEmployeeByName(employeeName) {
-  return employees.reduce((acc, cur) => {
-    if (employeeName === cur.firstName || employeeName === cur.lastName) {
-      return cur;
-    }
-    return acc;
-  }, {});
+  return employees.find((ind) => (employeeName === ind.firstName || employeeName === ind.lastName));
 }
 
 module.exports = getEmployeeByName;
+
+/* return employees.reduce((acc, cur) => {
+  if (employeeName === cur.firstName || employeeName === cur.lastName) {
+    return cur;
+  }
+  return acc;
+}, {}); */
+// console.log(getEmployeeByName('Stephanie'));
--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,14 @@
 const data = require('../data/zoo_data');
 
+const { species, employees } = data;
+
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  const firstBixo = employees.find((ind) => (ind.id === id)).responsibleFor[0];
+  const especie = species.find((qual) => (firstBixo === qual.id));
+  const quem = especie.residents.reduce((acc, cur) => ((acc.age > cur.age) ? acc : cur));
+  return [quem.name, quem.sex, quem.age];
 }
 
 module.exports = getOldestFromFirstSpecies;
+
+// console.log(getOldestFromFirstSpecies('c5b83cb3-a451-49e2-ac45-ff3f54fbe7e1'));
